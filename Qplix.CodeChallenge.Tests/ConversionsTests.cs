390c1b773ff872eb1f21c9278c43a6db2b64eb04
Guilherme Ferreira <guilhermeferreira@w4msolutions.com>
2025-05-02 14:21:27 +0100

    Commit missing files

---
+++ Qplix.CodeChallenge.Tests/ConversionsTests.cs
@@ -0,0 +1,15 @@
+﻿using Qplix.CodeChallenge.Utils;
+namespace Qplix.CodeChallenge.Tests;
+
+[TestFixture]
+public class ConversionsTests
+{
+    [Test]
+    public void ConversionTests_StringToDecimal_True()
+    {
+        var strNumber = "10".StringToDecimal();
+        
+        Assert.That(strNumber, Is.EqualTo(10));
+    }
+    
+}
--- Qplix.CodeChallenge/obj/Debug/net9.0/Qplix.CodeChallenge.AssemblyInfo.cs
+++ Qplix.CodeChallenge/obj/Debug/net9.0/Qplix.CodeChallenge.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Qplix.CodeChallenge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de14c2c1b16ad1ea29204bb6b26803270cdc94d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b604d12195a21a32d4da4fe2b8d592dad466b32b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Qplix.CodeChallenge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Qplix.CodeChallenge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
